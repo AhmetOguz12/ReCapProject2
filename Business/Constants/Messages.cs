0cb191014adbdad039cb406b71f3b17d7b2609b4
AhmetOguz12 <79849046+AhmetOguz12@users.noreply.github.com>
2021-09-21 16:24:53 +0300

    ReCarProjesi2

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -6,13 +6,13 @@ namespace Business.Constants
 {
      public static class Messages
     {
-        public static string CarsAdded = "Ürün Eklendi";
-        public static string CarNameInvalid = "Ürün İsmi Geçersiz";
-        public static string CarPriceInvalid = "Ürün Fiyatı Geçersiz";
-        public static string CarsDeleted = "Ürünler Silindi";
-        public static string CarsUpdated = "Ürünler Güncellendi";
+        public static string CarsAdded = "Araç Eklendi";
+        public static string CarNameInvalid = "Araç İsmi Geçersiz";
+        public static string CarPriceInvalid = "Araç Fiyatı Geçersiz";
+        public static string CarsDeleted = "Araç Silindi";
+        public static string CarsUpdated = "Araç Güncellendi";
         public static string MainenanceTime = "Sistem Bakımda";
-        public static string CarsListed = "Ürünler Lstelendi";
+        public static string CarsListed = "Araç Lstelendi";
 
         public static string BrandAdded = "Marka Eklendi";
         public static string BrandUpdated = "Marka Güncellendi";
@@ -21,5 +21,21 @@ namespace Business.Constants
         public static string ColorAdded = "Renk Eklendi";
         public static string ColorDeleted = "Renk Silindi";
         public static string ColorUpdated = "Renk Güncellendi";
+
+        public static string UsersListed = "Kullanıcılar Listelendi";
+        public static string UsersAdded = "Kullancı Eklendi";
+        public static string UsersDeleted = "Kullanıcı Silindi";
+        public static string UsersUpdated = "Kullanıcı Güncellendi";
+
+        public static string CustomersListed = "Müşteriler Listelendi";
+        public static string CustomersAdded = "Müşteri Eklendi";
+        public static string CustomersDeleted = "Müşteri Silindi";
+        public static string CustomersUpdated = "Müşteri Güncellendi";
+
+        public static string RentalsListed = "Kiralam Tarihleri Listelendi";
+        public static string RentlDateInvalid = "Kiralama Tarihi boş ";
+        public static string RentalAdded = "Kiralama Tarihi Eklendi";
+        public static string RentalDeleted = "KiralamaTarihi Silindi";
+        public static string RentalUpdated = "Kiralam Tarihi Güncellendi";
     }
 }
